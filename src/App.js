bc62345984d060815ce7a736c6fa694d057d0288
incredible92 <sulymanabdullahi@gmail.com>
2020-12-17 11:19:47 +0100

    feat: api&setUp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
+
+const api = {
+  key: "408bdbeea5e5803234bbc214be44b6c1",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <div className="search-box">
+          <input type="text" className="search-box" placeholder="search..." />
+        </div>
+
+      </section>
     </div>
   );
 }
